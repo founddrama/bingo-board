e296986a431cd8c92143c695266851250f23249d
founddrama <founddrama@users.noreply.github.com>
2021-12-09 21:15:03 +0000

    Building dist from $GITHUB_SHA 📦

--- dist/style/square-class-resolver.js
+++ dist/style/square-class-resolver.js
@@ -3,6 +3,8 @@ export default function squareClassResolver(phrase) {
     switch (phrase) {
         case FREE:
             return 'free-square';
+        case 'FOO':
+            return 'goo';
         default:
             return '';
     }
