acbbe9b6990c2726b2117202b8f18f9edeb1edb1
Rob Friesel <rob@founddrama.net>
2021-12-09 16:13:54 -0500

    just trying something here

--- src/style/square-class-resolver.ts
+++ src/style/square-class-resolver.ts
@@ -4,6 +4,8 @@ export default function squareClassResolver(phrase: string): string {
   switch (phrase) {
     case FREE:
       return 'free-square';
+    case 'FOO':
+      return 'goo';
     default:
       return '';
   }
