c7e185dfec353fb31b5102c6d82fb59d3f4012c0
founddrama <founddrama@users.noreply.github.com>
2021-12-10 03:00:07 +0000

    Building dist from bf78a15 📦

--- dist/components/Bingo.js
+++ dist/components/Bingo.js
@@ -1,7 +1,11 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import BingoSquare from './BingoSquare';
 import cyrb53 from '../utils/cyrb53-hash';
+import baseSquareClassResolver from '../style/square-class-resolver';
 function Bingo({ phrases, squareClassResolver }) {
+    if (!squareClassResolver) {
+        squareClassResolver = baseSquareClassResolver;
+    }
     return (_jsx("main", Object.assign({ className: "Bingo" }, { children: phrases.map(phrase => (_jsx(BingoSquare, { phrase: phrase, squareClassResolver: squareClassResolver }, cyrb53(phrase)))) }), void 0));
 }
 export default Bingo;
