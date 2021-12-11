a62e941953581ff98dbfad7aac17e8302773d198
founddrama <founddrama@users.noreply.github.com>
2021-12-11 04:01:16 +0000

    Building dist from a10d6d2 📦

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
 import Bingo from './components/Bingo';
-import getBingoPhrases from './utils/bingo-phrases';
 import squareClassResolver from './style/square-class-resolver';
 import './style/index.css';
-export { getBingoPhrases, squareClassResolver };
+export { squareClassResolver };
 export default Bingo;
 //# sourceMappingURL=index.js.map
